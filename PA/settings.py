3e1cbe7681ab09e90e3892f107aeec57b4a84647
dinesoft <amandine.bresil@free.fr>
2022-06-20 23:29:58 +0200

    t

--- PA/settings.py
+++ PA/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import django_heroku
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,6 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['https://supertestesgi.herokuapp.com/']
 
+DEBUG_COLLECTSTATIC=1
 
 # Application definition
 
@@ -50,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+MIDDLEWARE_CLASSES = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'PA.urls'
@@ -119,11 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/PA/assets/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "PA/static/PA",
     BASE_DIR / "PA/static/PA/assets",
+    BASE_DIR / "static/"
 ]
 
 # Default primary key field type
